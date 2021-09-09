26e814cd470f8828fb7599c6ce69911ff12056c8
Megalodon Penetration Testing Framework <90415832+Meg4lodon@users.noreply.github.com>
2021-09-10 00:28:30 +0300

    Update scaner.py

--- scaner.py
+++ scaner.py
@@ -1,17 +1,23 @@
-import socket
-import os
+#import libraries
+import socket  #conenct to target port
+import os      #make some system commands
 
-def open_ports(host, port):
+def open_ports(host, port):   #see if port is open
     s = socket.socket()
     try:
         s.connect((host, port))
     except:
-        return False
+        return False  #port is close
     else:
-        return True
+        return True   #port is open
 
+    # \033[32m = Green
+    # \033[37m = White
+    # \033[31m = Red
+    
 os.system('cls||clear')
-print(r"""
+print("\033[32m ")   #print ascii art
+print(r"""  
                  .-'''/.\
                  (_.--'  |
                   |  ==  |
@@ -32,10 +38,10 @@ print(r"""
                 |          |
                ||    /\    ||
                ||   ,  .   || """)
-host = input("Enter Target Mashine > ")
+host = input("\033[37mEnter Target Machine \033[32m > \033[37m ")
 print("|_HOST   PORT   STATE")
-for port in range(1, 1025):
+for port in range(1, 1025):  #scanning first 1024 ports '65535 in total'
     if open_ports(host, port):
-        print("| ", host, "  ",port, "\033[32m OPEN \033[37m")
+        print("| ", host, "  ",port, "\033[32m OPEN \033[37m") #display open ports
     else:
-        pass
+        pass   #dont print closed ports
